717cafb472d61ab62447b3925d016e3dfac549d4
Anthony <ajsawyer94@gmail.com>
2024-06-20 22:01:23 -0700

    changed url ending

--- js/nasa.js
+++ js/nasa.js
@@ -1,9 +1,20 @@
-const nasaAPI = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=SAxJ4hiPTsWdLwsX2lN0FsRfNTvtwCFOfWzMF2Lp';
+const api = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=SAxJ4hiPTsWdLwsX2lN0FsRfNTvtwCFOfWzMF2Lp';
 
-fetch(nasaAPI)
+fetch(api)
     .then(response => response.json())
     .then(data => {
 
         console.log(data);
+        const length = data.photos.length;
+        const marsPix = [];
+        
+        for (i = 0; i < 4; i ++) {
+            marsPix.push(data.photos[i].img_src);
+        }
+        console.log('This is the pix:', marsPix);
 
+        const element = document.getElementById('nasa');
+        element.src = data.photos[8].img_src;
+
+        
     })
