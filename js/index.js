ddf6529737f35f7c76370cf5f0c59e4358739fe5
Anthony Sawyer <anthony@Anthonys-MacBook-Pro.local>
2024-06-20 19:24:17 -0700

    fixed issues

--- js/index.js
+++
@@ -1,8 +0,0 @@
-let currentIndex = 0;
-const images = document.querySelectorAll('.imageant');
-
-function changeImage(direction) {
-    images[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + direction + images.length) % images.length;
-    images[currentIndex].classList.add('active');
-}
