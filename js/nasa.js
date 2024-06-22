4fe7bee5606982971214f212c40b59c644b7acb6
Anthony <ajsawyer94@gmail.com>
2024-06-21 19:37:35 -0700

    testing api hide

--- js/nasa.js
+++ js/nasa.js
@@ -1,22 +1,31 @@
 //const dotenv = require('dotenv');
 //const KEY = process.env.API_KEY
 //const api = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=SAxJ4hiPTsWdLwsX2lN0FsRfNTvtwCFOfWzMF2Lp`;
+
 const api = 'http://localhost:3000/api/data';
-fetch(api)
-    .then(response => response.json())
-    .then(data => {
+
+async function fetchData() {
+    try {
+        const response = await fetch(api);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
 
         console.log(data);
         const length = data.photos.length;
         const marsPix = [];
         
-        for (i = 0; i < 4; i ++) {
+        for (let i = 0; i < 4; i++) {
             marsPix.push(data.photos[i].img_src);
         }
         console.log('This is the pix:', marsPix);
 
         const element = document.getElementById('nasa');
         element.src = data.photos[8].img_src;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
 
-        
-    })
+fetchData();
