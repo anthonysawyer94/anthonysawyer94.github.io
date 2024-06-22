ccd8d6ac391887a5875ce23cab5c0398b53fe920
Anthony <ajsawyer94@gmail.com>
2024-06-21 18:40:58 -0700

    hide api

--- js/nasa.js
+++ js/nasa.js
@@ -1,5 +1,7 @@
-const api = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=SAxJ4hiPTsWdLwsX2lN0FsRfNTvtwCFOfWzMF2Lp';
-
+//const dotenv = require('dotenv');
+//const KEY = process.env.API_KEY
+//const api = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=SAxJ4hiPTsWdLwsX2lN0FsRfNTvtwCFOfWzMF2Lp`;
+const api = 'http://localhost:3000/api/data';
 fetch(api)
     .then(response => response.json())
     .then(data => {
