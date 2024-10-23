b30f0aafb8d916b1670608ca1fc107006e358f7d
Anthony <ajsawyer94@gmail.com>
2024-10-22 22:06:05 -0700

    changes

--- weather.php
+++ weather.php
@@ -1,5 +1,10 @@
 <?php
 
+// Allow requests only from your website
+if (strpos($_SERVER['HTTP_REFERER'], 'https://anthonyjsawyer.com') === false) {
+    die('Unauthorized access');
+}
+
 // Your OpenWeather API Key
 $apiKey = '186fce6232608c1c135ee734a641feb9';
 
@@ -14,6 +19,7 @@ $weatherData = file_get_contents($apiUrl);
 
 // Send the result back to the frontend
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: https://www.anthonyjsawyer.com");
 echo $weatherData;
 
 ?>
