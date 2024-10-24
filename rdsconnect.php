9adbffdbd79282d5fe8117d6b0184454292742b7
Anthony <ajsawyer94@gmail.com>
2024-10-24 03:29:34 -0700

    su

--- rdsconnect.php
+++ rdsconnect.php
@@ -12,6 +12,8 @@ $conn = new mysqli($host, $username, $password, $dbname);
 // Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully to the database!<br>"; // Success message
 }
 
 // Example: Fetching data from a table called 'users'
