f91b06844492c9720681bc1d4b209565d45c10fe
Anthony <ajsawyer94@gmail.com>
2024-10-25 00:05:05 -0700

    a

--- api/2048-hi-score.php
+++ api/2048-hi-score.php
@@ -1,4 +1,10 @@
 <?php
+
+//Error Messages if needed
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Database connection parameters
 $host = 'database-1.c7c8se6ww0t2.us-east-1.rds.amazonaws.com';
 $dbname = 'website';
