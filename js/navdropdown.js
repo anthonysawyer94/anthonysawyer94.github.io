83165163afb52f6e8eae36dae58815593a914828
Anthony <ajsawyer94@gmail.com>
2024-11-10 21:22:55 -0800

    debug

--- js/navdropdown.js
+++ js/navdropdown.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const gamesLink = document.getElementById('games');
+    console.log('games linkk', gamesLink);
     const dropdownContent = document.querySelector('.dropdown-content');
-  
+    console.log('dropdown', dropdownContent);
     gamesLink.addEventListener('click', function (event) {
       event.preventDefault(); // Prevent default link behavior
       dropdownContent.classList.toggle('show'); // Toggle the 'show' class to open/close the dropdown
