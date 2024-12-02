678e5cbe297f0122276527ffab3413ba5bc7a606
Anthony <ajsawyer94@gmail.com>
2024-12-02 00:50:25 -0800

    good

--- js/navbar.js
+++ js/navbar.js
@@ -41,3 +41,23 @@ document.addEventListener('DOMContentLoaded', async function () {
     }
   });
 });
+
+// Venmo link
+
+function isMobile() {
+  // Regular expression to match common mobile devices
+  return /Mobi|Android|iPhone|iPad|iPod|Windows Phone|BlackBerry/i.test(navigator.userAgent);
+}
+
+function handleVenmoLink() {
+    if (isMobile()) {
+        // If on mobile, use the venmo:// URL scheme
+        window.location.href = "venmo://paycharge?txn=pay&recipients=anthonysawyer";
+    } else {
+        // If on desktop, open the Venmo website link
+        window.location.href = "https://venmo.com/anthonysawyer";
+    }
+}
+
+// Add event listener to the button
+document.getElementById("venmo-button").addEventListener("click", handleVenmoLink);
