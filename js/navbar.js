8e8c626b6fa3153d15d43546c6e81083cee28d22
Anthony <ajsawyer94@gmail.com>
2024-12-02 18:40:49 -0800

    ff

--- js/navbar.js
+++ js/navbar.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', async function () {
   function handleVenmoLink() {
       if (isMobile()) {
           // If on mobile, use the venmo:// URL scheme
-          alert('you are on a mobile device')
+          //alert('you are on a mobile device')
           //window.location.href = "venmo://paycharge?txn=pay&recipients=anthonysawyer";
           window.location.href = "venmo://paycharge?txn=pay&recipients=anthonysawyer"; // just for now
       } else {
